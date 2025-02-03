9a3f9fa834bf385811d13bd3a7ec35e3c5015250
5asul <drafasul@gmail.com>
2025-02-04 00:44:39 +0300

    .

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,12 +6,31 @@ import messageRoutes from './routes/messageRoutes';
 import chatRoomRoutes from './routes/chatRoomRoutes';
 
 const app = express();
-
-app.use(cors({ origin: "https://ahmed-chat-io.vercel.app",
+const corsOptions = { 
+    origin: 'https://ahmed-chat-io.vercel.app', 
     optionsSuccessStatus: 200 ,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-}));
+    credentials: true,
+  };
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", 'true');
+    next()
+  })
+  app.options('*', cors(corsOptions)); // Enable preflight requests for all routes
 
 app.use('/api/auth', userRoutes);
 app.use('/api/messages', messageRoutes);
