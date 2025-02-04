d40817c24f9f1c75c1005cd5765f240eee4b143b
5asul <drafasul@gmail.com>
2025-02-04 13:44:34 +0300

    .

--- frontend/src/app/componants/Footer.tsx
+++ frontend/src/app/componants/Footer.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-
   return (
     <footer className={` bg-blue-500 text-white p-4 text-center `}>
-        &copy; 2023 Chat App. All rights reserved.
+      &copy; 2025 Ahmed Chat App. All rights reserved.
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
